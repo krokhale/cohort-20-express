bb3c7b1afb85b552e549fa3287f87d45714ae600
krokhale <krishna.rokhale@gmail.com>
2022-03-03 17:50:09 -0800

    checking in

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/something', function(req, res, next) {
+  // you are supposed to write code here that maybe fetches information from the DB
+  // once done
+
+  // console.log('the request object is', req)
+
+  setTimeout(() => {
+    res.send('respond with a resource');
+    res.json()
+  }, 5000)
+
+});
+
 module.exports = router;
